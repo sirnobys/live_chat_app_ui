dab3b4c7cdcaf8b236c093653fa3c3eab41bac7e
sirnobys <sirnobys@gmail.com>
2022-05-07 18:34:58 +0000

    config ui

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import io from 'socket.io-client'
 
 const socket = io.connect('https://live-chat-messaging2.herokuapp.com',{
   upgrade: false,
-  transports: ['websocket'],
+  transports: ['polling'],
   'reconnection': true,
   'reconnectionDelay': 500,
   'reconnectionAttempts': 10
